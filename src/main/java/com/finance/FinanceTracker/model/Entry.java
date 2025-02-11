6ff2fe37ebe5eaf2c1814cbcc6f6e887990b805f
Sankarshana <sankarshana2003@gmail.com>
2025-02-11 17:52:26 +0530

    Defined schema for entry

--- src/main/java/com/finance/FinanceTracker/model/Entry.java
+++ src/main/java/com/finance/FinanceTracker/model/Entry.java
@@ -1,4 +1,29 @@
 package com.finance.FinanceTracker.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Entry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double amount;
+    private String category;
+    private String type;
+    private LocalDate date;
+    private String description;
 }
